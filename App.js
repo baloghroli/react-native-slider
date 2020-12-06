548da3ec886a3004636098712118706c013b2fc4
Roland Balogh <balogh.roland33@gmail.com>
2020-12-06 21:43:22 +0100

    add slider

--- App.js
+++ App.js
@@ -1,21 +1,80 @@
-/**
- * Sample React Native App
- * https://github.com/facebook/react-native
- *
- * @format
- * @flow strict-local
- */
-
-import React from 'react';
-import {SafeAreaView, StyleSheet, Text} from 'react-native';
+import React, {useState, useRef} from 'react';
+import {
+  SafeAreaView,
+  StyleSheet,
+  Text,
+  View,
+  Animated,
+  PanResponder,
+} from 'react-native';
 
 const App = () => {
+  const [sliderDimensions, setSliderDimensions] = useState({
+    height: null,
+    start: null,
+    end: null,
+  });
+
+  const stepperAnim = useRef(new Animated.Value(0)).current;
+  const railFillAnim = useRef(new Animated.Value(0)).current;
+
+  const stepperResponder = PanResponder.create({
+    onStartShouldSetPanResponder: () => true,
+    onPanResponderGrant: () => {
+      stepperAnim.setOffset(stepperAnim._value);
+      railFillAnim.setOffset(railFillAnim._value);
+    },
+    onPanResponderMove: (evt, {dy, moveY}) => {
+      if (moveY > sliderDimensions.start && moveY < sliderDimensions.end) {
+        stepperAnim.setValue(dy);
+        railFillAnim.setValue(-dy);
+      }
+    },
+    onPanResponderRelease: () => {
+      stepperAnim.flattenOffset();
+      railFillAnim.flattenOffset();
+    },
+  });
+
   return (
-    <>
-      <SafeAreaView>
-        <Text style={styles.title}>What's your height?</Text>
-      </SafeAreaView>
-    </>
+    <SafeAreaView>
+      <Text style={styles.title}>What's your height?</Text>
+      <View
+        style={styles.slider}
+        onLayout={(evt) => {
+          const {height, y} = evt.nativeEvent.layout;
+          setSliderDimensions({
+            start: y,
+            height: height,
+            end: y + height,
+          });
+        }}>
+        <View style={styles.rail}>
+          <Animated.View style={[styles.railFill, {height: railFillAnim}]}>
+            {sliderDimensions.height
+              ? Array.apply(
+                  null,
+                  Array(Math.floor(sliderDimensions.height / 10)),
+                ).map((item, index) => (
+                  <View
+                    key={index}
+                    style={[styles.railFillSpace, {bottom: index * 10}]}
+                  />
+                ))
+              : null}
+          </Animated.View>
+        </View>
+        <Animated.View
+          {...stepperResponder.panHandlers}
+          style={[
+            styles.stepper,
+            {
+              transform: [{translateY: stepperAnim}],
+            },
+          ]}
+        />
+      </View>
+    </SafeAreaView>
   );
 };
 
@@ -23,7 +82,39 @@ const styles = StyleSheet.create({
   title: {
     textAlign: 'center',
     fontSize: 20,
-    marginTop: 50,
+    marginVertical: 50,
+  },
+  slider: {
+    width: 50,
+    height: '80%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    position: 'relative',
+    marginBottom: 50,
+  },
+  rail: {
+    width: 20,
+    height: '100%',
+    marginLeft: 'auto',
+    marginRight: 'auto',
+    backgroundColor: '#DBDBDB',
+  },
+  stepper: {
+    width: '100%',
+    height: 5,
+    backgroundColor: 'black',
+  },
+  railFill: {
+    width: '100%',
+    backgroundColor: '#CBAA71',
+    position: 'absolute',
+    bottom: 0,
+  },
+  railFillSpace: {
+    height: 5,
+    width: '100%',
+    backgroundColor: 'white',
+    position: 'absolute',
   },
 });
 
